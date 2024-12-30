fa8d1e77c90fbb6dbd44be68d897772ee5d3de33
zerocruch <34393956+zerocruch@users.noreply.github.com>
2024-12-30 16:59:31 +0100

    adding filter

--- api/data.js
+++ api/data.js
@@ -6,6 +6,11 @@ const COLLECTION_NAME = "backlogs";
 
 const endDate = new Date("2024-12-22T20:00:00Z");
 
+function filterPostsByDate(posts, date) {
+  const filterDate = new Date(date);
+  return posts.filter((post) => new Date(post.created_at) > filterDate);
+}
+
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
@@ -19,15 +24,26 @@ export default async function handler(req, res) {
 
       await client.close();
 
+      const { date } = req.body;
+
+      if (!date) {
+        return res.status(200).json({
+          data: messages,
+        });
+      } else {
+        const filteredPosts = filterPostsByDate(messages, date);
+        return res.status(200).json({
+          data: filteredPosts,
+        });
+      }
+
+      //const filteredPosts = filterPostsByDate(posts, '2024-12-30T16:50:00.000Z');
+
       /*const hasWon = messages.some((item) => item.won === true);
 
       const currentDate = new Date();
       console.log(currentDate);
       const gameOver = currentDate.getTime() >= endDate.getTime();*/
-
-      return res.status(200).json({
-        data: messages,
-      });
     } catch (error) {
       return res.status(500).json({ error: "Failed to fetch messages" });
     }
